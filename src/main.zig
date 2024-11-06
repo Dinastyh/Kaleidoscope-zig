12221797871baaad18b05e3f4f078cdb7b737d3c
Paul LEGE <paul.lege@protonmail.com>
2024-11-06 21:24:44 +0100

    feat(main): Start parserTopLevel for test

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,11 @@
 const std = @import("std");
+const Parser = @import("parser.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var parser = try Parser.init(gpa.allocator(), std.io.getStdIn().reader().any());
+    defer parser.deinit();
+
+    try parser.runTopLevel();
+}
